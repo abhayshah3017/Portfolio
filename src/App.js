36cf66594e38f6516c1f6ec76382a88669be4724
Abhay Shah <19bec119@nirmauni.ac.in>
2021-08-09 10:25:56 +0530

    Header Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
+import Header from './Components/header';
 import NavBar from './Components/navBar';
+import Particles from 'react-particles-js';
 
 function App() {
   return (
     <>
+    <Particles 
+      params={{
+        particles: {
+          number: {
+             value: 40,
+             density: {
+                enable: true,
+                value_area: 900
+                          }
+                      },
+          shape: {
+            type: "polygon",
+            stroke: {
+              width: 6,
+              color: "#f9ab00"
+            }
+          }
+                    }
+                 }}/>
     <NavBar />
-    
+    <Header />
     </>
   );
 }
