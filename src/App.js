99268c764d015ca3c25f523085f2c46aeaea8730
Abhay Shah <19bec119@nirmauni.ac.in>
2021-09-05 15:19:03 +0530

    Routes made and linked for every route

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
-import './App.css';
-import Header from './Components/header';
-import NavBar from './Components/navBar';
-import Particles from 'react-particles-js';
+import "./App.css";
+import Header from "./Components/header";
+import NavBar from "./Components/navBar";
+import Particles from "react-particles-js";
+import { Route, Redirect, BrowserRouter, Switch  } from "react-router-dom";
+import Profile from "./Components/profile";
 
 function App() {
   return (
-    <>
-    <Particles 
-      params={{
-        particles: {
-          number: {
-             value: 40,
-             density: {
+    <BrowserRouter>
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 40,
+              density: {
                 enable: true,
-                value_area: 900
-                          }
-                      },
-          shape: {
-            type: "polygon",
-            stroke: {
-              width: 6,
-              color: "#f9ab00"
-            }
-          }
-                    }
-                 }}/>
-    <NavBar />
-    <Header />
-    </>
+                value_area: 900,
+              },
+            },
+            shape: {
+              type: "polygon",
+              stroke: {
+                width: 6,
+                color: "#f9ab00",
+              },
+            },
+          },
+        }}
+      />
+      <NavBar />
+      <Switch>
+        <Route path="/home" exact component={Header}></Route>
+        <Route path="/profile" exact component={Profile}></Route>
+        <Redirect from="/" exact to="/home" />
+        <Redirect to="/notFound" />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
